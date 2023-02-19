9e3533f78e67aa4c2fad9727fbf2306327dec2de
Chris Ellepola <cellepo@gmail.com>
2023-02-18 19:55:22 -0800

    commons-csv-1.10.0 - cleanup default Test file

--- Ledger_csv_to_TaxBit/src/test/java/org/example/Ledger_csv_to_TaxBitTest.java
+++ Ledger_csv_to_TaxBit/src/test/java/org/example/Ledger_csv_to_TaxBitTest.java
@@ -4,12 +4,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class Ledger_csv_to_TaxBitTest
-    extends TestCase
-{
+public class Ledger_csv_to_TaxBitTest extends TestCase {
+
     /**
      * Create the test case
      *
@@ -28,11 +24,4 @@ public class Ledger_csv_to_TaxBitTest
         return new TestSuite( Ledger_csv_to_TaxBitTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
