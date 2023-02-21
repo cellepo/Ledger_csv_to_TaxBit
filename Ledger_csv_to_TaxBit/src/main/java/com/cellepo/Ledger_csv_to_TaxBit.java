3fb3b8e28df2ca5cb7bdb92114fce5181cf4fd2e
Chris Ellepola <cellepo@gmail.com>
2023-02-20 16:21:30 -0800

    command_line_args - Pass inFilename on command line.

--- Ledger_csv_to_TaxBit/src/main/java/com/cellepo/Ledger_csv_to_TaxBit.java
+++ Ledger_csv_to_TaxBit/src/main/java/com/cellepo/Ledger_csv_to_TaxBit.java
@@ -4,17 +4,18 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Ledger_csv_to_TaxBit {
 
-    static final String IN_CSV_FILENAME = "in.csv";
     static final String[] OUT_HEADER_NAMES = {"outA", "outB"};
 
     public static void main(String[] args) {
-        String outFilename = args[0];
+        String inFilename = args[0];
+        String outFilename = args[1];
         // without a try-with-resources (which automatically closes), must printer.close()
         try(
             CSVPrinter csvPrinter = new CSVPrinter(
@@ -22,13 +23,20 @@ public class Ledger_csv_to_TaxBit {
                 CSVFormat.Builder.create().setHeader(OUT_HEADER_NAMES).build()
             )
         ){
-            Iterable<CSVRecord> rowsIn = CSVFormat.Builder.create().setHeader().build()
-                .parse(new FileReader(IN_CSV_FILENAME));
+            Iterable<CSVRecord> rowsIn;
+            try {
+                rowsIn = CSVFormat.Builder.create().setHeader().build().parse(new FileReader(inFilename));
+
+            } catch(FileNotFoundException fnfe){
+                System.out.println("inFilename arg not found: \"" + inFilename + "\"");
+                return;
+            }
             for(CSVRecord row: rowsIn){
                 csvPrinter.printRecord((Object[]) row.values());
             }
 
         } catch (IOException ioe) {
+            System.out.println("Error creating either FileWriter, CSVPrinter, or FileReader");
             ioe.printStackTrace();
         }
     }
